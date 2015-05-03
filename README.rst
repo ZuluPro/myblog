0d454f1635cda2330e6d0be85a64849f1c5ef60e
ZuluPro <montheanthony@hotmail.com>
2015-05-03 16:29:46 +0200

    Improve setup.py

--- README.rst
+++ README.rst
@@ -1,3 +1,11 @@
 ===================
 My blog source code
 ===================
+
+
+Installation
+============
+
+
+Testing
+=======
---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+import myblog
+
+setup(
+    name='myblog',
+    version=myblog.__version__,
+    description='',
+    long_description=open(os.path.join('README.rst')).read(),
+    keywords='django, zinnia, blog',
+    author=myblog.__author__,
+    author_email=myblog.__email__,
+    url=myblog.__url__,
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license=myblog.__license__,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=open('requirements.txt').read().splitlines()
+)
