8f8110bff738be02db39d63a601798ec8ff23aab
ZuluPro <montheanthony@hotmail.com>
2016-03-28 20:00:07 -0400

    Externalize Flickr Storage

--- myblog/photos/views.py
+++ myblog/photos/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from django.conf import settings
-from photos.storage import FlickrStorage
+from flickr_pony.storage import get_flickr_storage
 
 
 @cache_page(60*30)
 def photos(request):
-    storage = FlickrStorage(**settings.FLICKR_STORAGE_OPTIONS)
-    photos = storage.list_image_and_thumb(size='m')
+    storage = get_flickr_storage()
+    if storage.user_id:
+        photos = storage.list_image_and_thumb(size='m')
+    else:
+        photos = []
     return render(request, 'photos.html', {
+        'title': "Photo gallery",
         'photos': photos,
     })
